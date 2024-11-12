ccfcfd84cf91a208b51c5e0740798fc33c89a533
itsjaee28 <164783308+itsjaee28@users.noreply.github.com>
2024-11-13 02:11:53 +0800

    Update errorhandling.sol

--- Module 3/errorhandling.sol
+++ Module 3/errorhandling.sol
@@ -1,69 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-/*
-Functionality
-Contract successfully uses require()
-Contract successfully uses assert()
-Contract successfully uses revert() statements
-*/
+contract ErrorHandling {
+    uint public balance;
 
-contract MyToken {
-
-    constructor(){
-        owner = msg.sender;
+    // Function to deposit funds
+    function deposit(uint _amount) public {
+        require(_amount > 50, "Deposit amount must be greater than 50 pesos.");
+        balance += _amount;
     }
 
-    // public variables here
-    string public name = "simple";
-    string public symbol = "SMPL";
-    uint public totalSupply = 0;
-    address public owner;
-
-    //emits Events
-    event Mint(address indexed to, uint amount);
-    event Burn(address indexed from, uint amount); 
-    event Transfer(address indexed from, address indexed to, uint amount);
-
-    //errors
-    error NoEnoughBalance(uint balance, uint withdrawamount);
-
-    // mapping variable here
-    mapping(address => uint) public balances;
-
-    //modifiers
-    modifier Owner{
-        assert (msg.sender == owner);
-        _;
-    }
-
-    // mint function
-    function mint(address _address, uint _value) public Owner{
-        totalSupply += _value;
-        balances[_address] += _value;
-        emit Mint(_address, _value);
-    }
-
-    // burn function
-    function burn (address _address, uint _value) public Owner{
-        if(balances[_address] >= _value){
-            revert NoEnoughBalance({balance: balances[_address], withdrawamount: _value});
-        }
-        else{
-            totalSupply -= _value;
-            balances[_address] -= _value;
-            emit Burn(_address, _value);
+    // Function to withdraw funds
+    function withdraw(uint _amount) public {
+        // Ensure that the balance is sufficient for the withdrawal
+        if (_amount > balance) {
+            revert("Insufficient balance for withdrawal.");
         }
 
+        balance -= _amount;
+        // Assert that balance will not go negative after withdrawal
+        assert(balance >= 0);
     }
 
-    //transfer
-    function transfer (address _receiver, uint _value) public{
-        require(balances[msg.sender]>= _value, "Account balance must be greater than transferred value");
-        balances[msg.sender] -= _value;
-        balances[_receiver] += _value;
-        emit Transfer(msg.sender, _receiver, _value);
-
+    // Check balance
+    function getBalance() public view returns (uint) {
+        return balance;
     }
-
 }
